2acf0dda3dc0ff289220aaa093f50eb34728b048
rowahone614 <rowahone614@SNS-D-130S0R>
2019-10-30 13:42:47 -0300

    Add project files.

---
+++ summative-choice-game/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace summative_choice_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pos1 = new System.Windows.Forms.PictureBox();
+            this.pos11 = new System.Windows.Forms.PictureBox();
+            this.pos10 = new System.Windows.Forms.PictureBox();
+            this.pos9 = new System.Windows.Forms.PictureBox();
+            this.pos8 = new System.Windows.Forms.PictureBox();
+            this.pos7 = new System.Windows.Forms.PictureBox();
+            this.pos6 = new System.Windows.Forms.PictureBox();
+            this.pos5 = new System.Windows.Forms.PictureBox();
+            this.pos4 = new System.Windows.Forms.PictureBox();
+            this.pos3 = new System.Windows.Forms.PictureBox();
+            this.pos2 = new System.Windows.Forms.PictureBox();
+            this.ifLbl = new System.Windows.Forms.Label();
+            this.sceneLabel = new System.Windows.Forms.Label();
+            this.chooseLabel = new System.Windows.Forms.Label();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pos1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(-6, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(682, 113);
+            this.label1.TabIndex = 0;
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.ForestGreen;
+            this.greenButton.Location = new System.Drawing.Point(144, 173);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(75, 166);
+            this.greenButton.TabIndex = 1;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.GreenButton_Click);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.DarkRed;
+            this.redButton.Location = new System.Drawing.Point(293, 173);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(75, 166);
+            this.redButton.TabIndex = 2;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.RedButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.blueButton.Location = new System.Drawing.Point(445, 173);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(75, 166);
+            this.blueButton.TabIndex = 3;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.BlueButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(59, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(548, 10);
+            this.label2.TabIndex = 4;
+            // 
+            // pos1
+            // 
+            this.pos1.BackColor = System.Drawing.Color.Transparent;
+            this.pos1.BackgroundImage = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos1.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos1.Location = new System.Drawing.Point(59, 40);
+            this.pos1.Name = "pos1";
+            this.pos1.Size = new System.Drawing.Size(34, 33);
+            this.pos1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos1.TabIndex = 5;
+            this.pos1.TabStop = false;
+            // 
+            // pos11
+            // 
+            this.pos11.Image = global::summative_choice_game.Properties.Resources.runningguy;
+            this.pos11.Location = new System.Drawing.Point(575, 40);
+            this.pos11.Name = "pos11";
+            this.pos11.Size = new System.Drawing.Size(32, 33);
+            this.pos11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos11.TabIndex = 15;
+            this.pos11.TabStop = false;
+            // 
+            // pos10
+            // 
+            this.pos10.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos10.Location = new System.Drawing.Point(528, 40);
+            this.pos10.Name = "pos10";
+            this.pos10.Size = new System.Drawing.Size(30, 33);
+            this.pos10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos10.TabIndex = 14;
+            this.pos10.TabStop = false;
+            // 
+            // pos9
+            // 
+            this.pos9.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos9.Location = new System.Drawing.Point(478, 40);
+            this.pos9.Name = "pos9";
+            this.pos9.Size = new System.Drawing.Size(30, 33);
+            this.pos9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos9.TabIndex = 13;
+            this.pos9.TabStop = false;
+            // 
+            // pos8
+            // 
+            this.pos8.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos8.Location = new System.Drawing.Point(426, 40);
+            this.pos8.Name = "pos8";
+            this.pos8.Size = new System.Drawing.Size(30, 33);
+            this.pos8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos8.TabIndex = 12;
+            this.pos8.TabStop = false;
+            // 
+            // pos7
+            // 
+            this.pos7.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos7.Location = new System.Drawing.Point(375, 40);
+            this.pos7.Name = "pos7";
+            this.pos7.Size = new System.Drawing.Size(30, 33);
+            this.pos7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos7.TabIndex = 11;
+            this.pos7.TabStop = false;
+            // 
+            // pos6
+            // 
+            this.pos6.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos6.Location = new System.Drawing.Point(324, 40);
+            this.pos6.Name = "pos6";
+            this.pos6.Size = new System.Drawing.Size(30, 33);
+            this.pos6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos6.TabIndex = 10;
+            this.pos6.TabStop = false;
+            // 
+            // pos5
+            // 
+            this.pos5.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos5.Location = new System.Drawing.Point(272, 40);
+            this.pos5.Name = "pos5";
+            this.pos5.Size = new System.Drawing.Size(30, 33);
+            this.pos5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos5.TabIndex = 9;
+            this.pos5.TabStop = false;
+            // 
+            // pos4
+            // 
+            this.pos4.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos4.Location = new System.Drawing.Point(218, 40);
+            this.pos4.Name = "pos4";
+            this.pos4.Size = new System.Drawing.Size(30, 33);
+            this.pos4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos4.TabIndex = 8;
+            this.pos4.TabStop = false;
+            // 
+            // pos3
+            // 
+            this.pos3.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos3.Location = new System.Drawing.Point(164, 40);
+            this.pos3.Name = "pos3";
+            this.pos3.Size = new System.Drawing.Size(30, 33);
+            this.pos3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos3.TabIndex = 7;
+            this.pos3.TabStop = false;
+            // 
+            // pos2
+            // 
+            this.pos2.Image = global::summative_choice_game.Properties.Resources.clown2;
+            this.pos2.Location = new System.Drawing.Point(110, 40);
+            this.pos2.Name = "pos2";
+            this.pos2.Size = new System.Drawing.Size(30, 33);
+            this.pos2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pos2.TabIndex = 6;
+            this.pos2.TabStop = false;
+            // 
+            // ifLbl
+            // 
+            this.ifLbl.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ifLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ifLbl.Location = new System.Drawing.Point(572, 95);
+            this.ifLbl.Name = "ifLbl";
+            this.ifLbl.Size = new System.Drawing.Size(73, 65);
+            this.ifLbl.TabIndex = 16;
+            this.ifLbl.Text = "If he gets here, you die";
+            // 
+            // sceneLabel
+            // 
+            this.sceneLabel.BackColor = System.Drawing.Color.DimGray;
+            this.sceneLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sceneLabel.Location = new System.Drawing.Point(160, 345);
+            this.sceneLabel.Name = "sceneLabel";
+            this.sceneLabel.Size = new System.Drawing.Size(363, 115);
+            this.sceneLabel.TabIndex = 17;
+            this.sceneLabel.Text = "You awake in a strange room. You hear distant foot steps, you must choose a door " +
+    "to go through.";
+            this.sceneLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // chooseLabel
+            // 
+            this.chooseLabel.BackColor = System.Drawing.Color.DimGray;
+            this.chooseLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chooseLabel.Location = new System.Drawing.Point(556, 358);
+            this.chooseLabel.Name = "chooseLabel";
+            this.chooseLabel.Size = new System.Drawing.Size(104, 66);
+            this.chooseLabel.TabIndex = 18;
+            this.chooseLabel.Text = "Click on a door to choose.";
+            this.chooseLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.BackColor = System.Drawing.Color.LightGray;
+            this.promptLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(235, 98);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(193, 72);
+            this.promptLabel.TabIndex = 19;
+            this.promptLabel.Text = "Prompts will appear here.";
+            this.promptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 345);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.chooseLabel);
+            this.Controls.Add(this.sceneLabel);
+            this.Controls.Add(this.ifLbl);
+            this.Controls.Add(this.pos1);
+            this.Controls.Add(this.pos11);
+            this.Controls.Add(this.pos10);
+            this.Controls.Add(this.pos9);
+            this.Controls.Add(this.pos8);
+            this.Controls.Add(this.pos7);
+            this.Controls.Add(this.pos6);
+            this.Controls.Add(this.pos5);
+            this.Controls.Add(this.pos4);
+            this.Controls.Add(this.pos3);
+            this.Controls.Add(this.pos2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "                                                                                 " +
+    "                                                                          ";
+            ((System.ComponentModel.ISupportInitialize)(this.pos1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pos1;
+        private System.Windows.Forms.PictureBox pos2;
+        private System.Windows.Forms.PictureBox pos3;
+        private System.Windows.Forms.PictureBox pos4;
+        private System.Windows.Forms.PictureBox pos5;
+        private System.Windows.Forms.PictureBox pos6;
+        private System.Windows.Forms.PictureBox pos7;
+        private System.Windows.Forms.PictureBox pos8;
+        private System.Windows.Forms.PictureBox pos9;
+        private System.Windows.Forms.PictureBox pos10;
+        private System.Windows.Forms.PictureBox pos11;
+        private System.Windows.Forms.Label ifLbl;
+        private System.Windows.Forms.Label sceneLabel;
+        private System.Windows.Forms.Label chooseLabel;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ summative-choice-game/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace summative_choice_game
+{
+    public partial class Form1 : Form
+    {
+        int killerPosition = 0;
+        int scene = 0;
+        int num;
+        Random rGen = new Random();
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+
+            pos1.Visible = true;
+            pos2.Visible = false;
+            pos3.Visible = false;
+            pos4.Visible = false;
+            pos5.Visible = false;
+            pos6.Visible = false;
+            pos7.Visible = false;
+            pos8.Visible = false;
+            pos9.Visible = false;
+            pos10.Visible = false;
+
+            if (killerPosition == 1)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = true;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 2)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = true;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 3)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = true;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 4)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = true;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+
+            if (killerPosition == 5)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = true;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 6)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = true;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 7)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = true;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 8)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = true;
+                pos9.Visible = false;
+                pos10.Visible = false;
+            }
+
+            if (killerPosition == 9)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = true;
+                pos10.Visible = false;
+            }
+            if (killerPosition == 10)
+            {
+
+                pos1.Visible = false;
+                pos2.Visible = false;
+                pos3.Visible = false;
+                pos4.Visible = false;
+                pos5.Visible = false;
+                pos6.Visible = false;
+                pos7.Visible = false;
+                pos8.Visible = false;
+                pos9.Visible = false;
+                pos10.Visible = true;
+            }
+
+            promptLabel.Visible = true;
+            ifLbl.Visible = true;
+            chooseLabel.Visible = true;
+        }
+        private void GreenButton_Click(object sender, EventArgs e)
+        {
+            if (killerPosition == 12) { scene = 99; }
+
+            promptLabel.Visible = false;
+            ifLbl.Visible = false;
+            chooseLabel.Visible = false;
+
+            if (scene == 0) { scene = 1; }
+            else if (scene == 1) { scene = 3; }
+            else if (scene == 3) { scene = 2; }
+            else if (scene == 2) { scene = 4; }
+            else if (scene == 4) { scene = 12; }
+            else if (scene == 12) { scene = 10; }
+            else if (scene == 10) { scene = 22; }
+            else if (scene == 8) { scene = 10; }
+            else if (scene == 6)
+            {
+                num = rGen.Next(1, 101);
+                if (num >= 70)
+                { scene = 7; }
+                else { scene = 9; }
+            }
+            else if (scene == 7) { scene = 13; }
+            else if (scene == 11) { scene = 14; }
+            else if (scene == 14) { scene = 16; }
+            else if (scene == 5) { scene = 6; }
+
+
+            //if (scene == 99 )
+            //{
+            //    sceneLabel.Text = "Game Over!";
+            //    Thread.Sleep(2000);
+            //    sceneLabel.Text = "Play again? (Green door for yes | Blue door for no)";
+            //}
+        }
+
+        private void RedButton_Click(object sender, EventArgs e)
+        {
+            promptLabel.Visible = false;
+            ifLbl.Visible = false;
+            chooseLabel.Visible = false;
+
+            if (scene == 0) { scene = 2; }
+            else if (scene == 2) { scene = 6; }
+            else if (scene == 6) { scene = 19; }
+            else if (scene == 19) { scene = 13; }
+            else if (scene == 7) { scene = 15; }
+            else if (scene == 11) { scene = 17; }
+            else if (scene == 14) { scene = 15; }
+            else if (scene == 3) { scene = 6; }
+            else if (scene == 1) { scene = 2; }
+            else if (scene == 12) { scene = 22; }
+            else if (scene == 10) { scene = 22; }
+            else if (scene == 4) { scene = 6; }
+            else if (scene == 8) { scene = 10; }
+            else if (scene == 5) { scene = 6; }
+
+            ScenceOutput();
+        }
+
+        private void BlueButton_Click(object sender, EventArgs e)
+        {
+            promptLabel.Visible = false;
+            ifLbl.Visible = false;
+            chooseLabel.Visible = false;
+
+            if (scene == 0) { scene = 3; }
+            else if (scene == 3) { scene = 5; }
+            else if (scene == 5) { scene = 6; }
+            else if (scene == 6) { scene = 20; }
+            else if (scene == 2) { scene = 5; }
+            else if (scene == 1) { scene = 2; }
+            else if (scene == 4) { scene = 8; }
+            else if (scene == 8) { scene = 12; }
+            else if (scene == 12) { scene = 22; }
+            else if (scene == 10) { scene = 23; }
+            else if (scene == 7) { scene = 11; }
+            else if (scene == 11) { scene = 17; }
+            else if (scene == 14) { scene = 18; }
+            else if (scene == 19) { scene = 15; }
+
+            ScenceOutput();
+        }
+
+        private void ScenceOutput()
+        {
+            
+        }
+      
+    }
+}
